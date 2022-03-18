81c8739d7d481d627cf6eddae094e8b72777146d
Gabriel Cabanes <gcabanes@meilleursagents.com>
2022-03-18 14:52:27 +0100

    chore(config): relative path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src/'),
+      },
+    ],
+  },
 });
