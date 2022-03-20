6b3ca7ed5c608b15633d9d30e8c30fee62f125d2
Gabriel Cabanes <gcabanes@meilleursagents.com>
2022-03-20 12:46:44 +0100

    refactor(archi): split between containers and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Board from '@/components/Board';
-import Sidebar from '@/components/Sidebar';
+import Board from '@/container/Board';
+import Sidebar from '@/container/Sidebar';
 
 function App() {
   return (
