4ea3d43a6336817fa1dbc425333dac460bf61216
Gabriel Cabanes <gcabanes@meilleursagents.com>
2022-03-21 08:05:27 +0100

    feat(route): redirection on bad url / removed favicon call

--- src/containers/Board/Board.tsx
+++ src/containers/Board/Board.tsx
@@ -1,7 +1,7 @@
 import AddIcon from '@mui/icons-material/Add';
 import { Typography } from '@mui/material';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import BoardHeader from '@/components/BoardHeader';
 import Group from '@/components/Group';
@@ -10,11 +10,13 @@ import { useGetBoardGroupsQuery } from '@/graphql/generated';
 import * as S from './board.styles';
 
 const Board = () => {
+  const navigate = useNavigate();
   const { pathname } = useLocation();
   const currentBoardName = pathname.split('/').at(-1)?.replace('-', ' ') as string;
   const { data } = useGetBoardGroupsQuery({ variables: { name: currentBoardName } });
 
   if (!data) return null;
+  if (!data.board) navigate('/Design');
 
   return (
     <S.Wrapper>
